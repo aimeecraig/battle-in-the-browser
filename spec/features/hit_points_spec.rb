f8e2c8850bac80d3ab6bedb2d359de062a71ee3f
Aimée Craig <aimeecraig1@gmail.com>
2018-10-16 17:54:57 +0100

    hit points feature test initialized

---
+++ spec/features/hit_points_spec.rb
@@ -0,0 +1,9 @@
+feature 'View hit points' do
+  scenario '' do
+    visit('/')
+    fill_in :player_1_name, with: 'Dave'
+    fill_in :player_2_name, with: 'Mittens'
+    click_button 'Submit'
+    expect(page).to have_content 'Mittens: 100HP'
+  end
+end
